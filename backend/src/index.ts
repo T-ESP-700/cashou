7d8bdb154a367091df535cc638b38d14224ee9e3
Nafissatou Sylla <nafissatou.sylla@epitech.eu>
2025-07-21 15:02:46 +0200

    T-81/enums: Refactor router structure and update Dockerfile to generate Prisma client

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,5 @@
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
-import {appRouter} from "./server/routers";
+import {appRouter} from "./trcp/routers/app.router.ts";
 
 const server = Bun.serve({
     port: process.env.PORT || 3000,
