ae3e961a4bfc50a500f37f3c49a505f44dcc2e7c
Flavie Guiselin <flavie.sola.guiselin@gmail.com>
2025-08-26 11:19:14 +0200

    T-83 feat: add submarket API with CRUD operations and doc bruno

--- backend/src/routers/app.router.ts
+++ backend/src/routers/app.router.ts
@@ -2,6 +2,7 @@
 import { initTRPC } from "@trpc/server";
 import {levelRouter} from "./level.router.ts";
 import {marketRouter} from "./market.router.ts";
+import {submarketRouter} from "./submarket.router.ts";
 
 // Initialisation de tRPC - Framework pour créer des APIs type-safe
 const t = initTRPC.create();
@@ -10,6 +11,7 @@ const t = initTRPC.create();
 export const appRouter = t.router({
     level: levelRouter,
     market: marketRouter,
+    submarket: submarketRouter,
 });
 
 // Export du type pour utilisation côté frontend (type-safety)
